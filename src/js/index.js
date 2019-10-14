936140473024a04e00ec25d6cf231d95c88a6292
KayRago <kayleighrago@gmail.com>
2019-10-14 14:57:46 -0700

    installing NPM and other tools necessary to start the project, making first API calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 // Global app controller
+
+//https://www.food2fork.com/api/search
+//6fef1ceee29fc932e1ee88651708a4ed
+
+import Search from './Search';
+
+const search = new Search('pizza');
+console.log(search);
+
+search.getResults();
+
