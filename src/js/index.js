d14f04401847db6e64b2530a6949740b194297be
KayRago <kayleighrago@gmail.com>
2019-10-16 00:20:39 -0700

    built search view and loading spinner

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,71 @@
 //6fef1ceee29fc932e1ee88651708a4ed
 
 import Search from './Search';
+import * as searchView from './searchView';
+import { elements, renderLoader, clearLoader } from './base';
 
-const search = new Search('pizza');
-console.log(search);
+// global state of the app:
+//search object
+//current recipe object
+// shopping list object
+// liked recipes
+const state = {};
 
-search.getResults();
+const controlSearch = async () => {
+    // 1. get query from view
+    const query = searchView.getInput();
+    console.log(query);
+    
+    if (query) {
+        // 2. new search object and add to state
+        state.search = new Search(query);
+        
+        // 3. prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+        
+        // 4. search for recipes
+        await state.search.getResults();
+        
+        // 5. render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+}
+
+    elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
